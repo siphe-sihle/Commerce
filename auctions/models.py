d91140fef168bb608215e596e9a4be7e5ee5a2f9
siphe-sihle <sphe.images@gmail.com>
2022-06-13 11:19:35 +0200

    Created Listing, Bid, Comment, Category Models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,33 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length= 30)
+    description = models.CharField(max_length= 200)
+    current_bid = models.FloatField(default= 0.00, null= False, blank = True)
+    #Define User object that posted the listing
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= "listed_by")
+    #define a many to many relationship to bids
+    bids = models.ManyToManyField("Bid", blank= True, related_name= "bidders")
+
+    #Define many to many relationship to both comments and categories
+    comment = models.ManyToManyField("Comment", blank= True, related_name= "Commented_by")
+
+    def __str__(self):
+        return f"id: {self.id}, Title: {self.title}, Description:{self.description}"
+
+class Bid(models.Model):
+    price = models.FloatField(default= 0.00, null= False)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= "bidder")
+    listing = models.ForeignKey(Listing, on_delete= models.CASCADE, related_name= "bidded_listed")
+
+    def __str__(self):
+        return f"id: {self.id} user: {self.user} Listing: {self.listing}"
+
+class Comment(models.Model):
+    description = models.CharField(max_length= 200)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= "commenter")
+
+    def __str__(self):
+        return f"Description: {self.description}, user: {self.user}."
